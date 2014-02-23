5cb612255a0da46083b78e99f27a097547c8c4bd
TAC <tac@tac42.net>
2014-02-23 18:30:43 +0900

    メモリ操作，文字列操作の標準関数を実装

--- lib.c
+++ lib.c
@@ -19,3 +19,59 @@ int puts(unsigned char *str)
 	}
 	return 0;
 }
+
+void * memset(void *s, int c, size_t n){
+	for( int i = 0; i < n; i++ ){
+		char *dst = (char*)s;
+		*(dst + i) = c;
+	}
+	return s;
+}
+
+void * memcpy(void *dest, const void *src, size_t n){
+	for( int i = 0; i < n; i++ ){
+		const char *s = (const char*)src;
+		char *d = (char*)dest;
+		*(d + i) = *(s + i);
+	}
+	return dest;
+}
+
+int memcmp(const void *s1, const void *s2, size_t n){
+	for( int i = 0; i < n; i++ ){
+		const char *src1 = ((const char*)s1) + i;
+		const char *src2 = ((const char*)s2) + i;
+
+		if( *src1 == '\0' ) return 0;
+		if( *src2 == '\0' ) return 0;
+
+		const int diff = *src1 - *src2;
+		if( diff != 0 ) return diff;
+	}
+	return 0;
+}
+
+size_t strlen(const char *s){
+	int len = 0;
+	while( *(s + len) != '\0' ) len++;
+	return len;
+}
+
+char *strcpy(char *dest, const char *src){
+	while( *src != '\0' ){
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	return dest;
+}
+
+int strcmp(const char *s1, const char *s2){
+	int n1 = strlen(s1);
+	int n2 = strlen(s2);
+	return memcmp(s1, s2, n1 < n2 ? n1 : n2 );
+}
+
+int strncmp(const char *s1, const char *s2, size_t n){
+	return memcmp(s1, s2, n);
+}
--- lib.h
+++ lib.h
@@ -1,7 +1,17 @@
 #ifndef _LIB_H_INCLUDED_
 #define _LIB_H_INCLUDED_
 
+typedef long size_t;
+
 int putc(unsigned char c);   /** 1文字送信  */
 int puts(unsigned char *str);/** 文字列送信 */
+
+void * memset(void *s, int c, size_t n);
+void * memcpy(void *dest, const void *src, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+size_t strlen(const char *s);
+char *strcpy(char *dest, const char *src);
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
