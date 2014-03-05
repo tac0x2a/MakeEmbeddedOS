5fa4e61804317bdb226a9cd6ecc3f0af76ffe8ef
TAC <tac@tac42.net>
2014-03-06 02:16:11 +0900

    ROM領域に置かれていた.dataセクションと.bssセクションをRAM上に置いた．
    これで静的に確保された変数に書き込めるはずだったが，
    Flashに書き込むべきデータの書き込み先アドレスが，RAM上になってるため，
    ``Address is out of range. Skipping..`` みたいなエラーが出て書き込みに失敗する．

--- main.c
+++ main.c
@@ -2,13 +2,20 @@
 #include "serial.h"
 #include "lib.h"
 
+int hoge;
+volatile int fuga = 128;
+static int s_hoge;
+static int s_fuga = 128;
+
 int main(void)
 {
 	serial_init(SERIAL_DEFAULT_DEVICE);
 
-	puts( "hoge!\n");
-	putxval(0x2a, 4); puts("\n");
-	puts( "Hello World!\n");
+	puts( "fuga!\n");
+	putxval(fuga, 4); puts("\n");
+	fuga++;
+	putxval(fuga, 4); puts("\n");
+
 	while(1);
 	return 0;
 }
