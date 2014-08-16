2a7a7ac31ee6a15a59a5003f198bafe0eea1a3c4
TAC <tac@tac42.net>
2014-08-17 02:03:51 +0900

    テストのビルド時に警告が出まくるので修正
    lib.cで標準関数と同名の関数を再定義しているため。

--- test/test_lib.c
+++ test/test_lib.c
@@ -1,11 +1,17 @@
 #include "unity.h"
 #include "unity_fixture.h"
 
-#define getc   kz_getc
-#define gets   kz_gets
-#define putc   kz_putc
-#define puts   kz_puts
-#define size_t kz_size_t
+#define getc    kz_getc
+#define gets    kz_gets
+#define putc    kz_putc
+#define puts    kz_puts
+#define size_t  kz_size_t
+#define strncmp kz_strncmp
+#define strlen  kz_strlen
+#define memcmp  kz_memcmp
+#define memset  kz_memset
+#define memcpy  kz_memcpy
+
 
 #undef NULL
 
