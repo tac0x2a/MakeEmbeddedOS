738b7fcb901f8941c594f916fdd98ba1904a9159
TAC <tac@tac42.net>
2014-08-20 23:35:09 +0900

    タブは半角スペース2つ。

--- main.c
+++ main.c
@@ -6,106 +6,106 @@
 
 static int init(){
 
-	/* リンカスクリプトで定義されているシンボル */
+  /* リンカスクリプトで定義されているシンボル */
   extern int text_start,   etext;
   extern int rodata_start, erodata;// ROM上のデータ
-	extern int data_start,   edata;  // 初期値のある静的データ
-	extern int bss_start,    ebss;   // 初期値のない静的データデータ
-	extern int stack;
+  extern int data_start,   edata;  // 初期値のある静的データ
+  extern int bss_start,    ebss;   // 初期値のない静的データデータ
+  extern int stack;
 
-	/** データ領域とBSS領域を初期化する */
-	const long data_length = (long)&edata - (long)&data_start; // データ領域のサイズ
-	memcpy(&data_start, &erodata, data_length); // ROM領域中の rom の終端からRAMまでごっそりコピーする
+  /** データ領域とBSS領域を初期化する */
+  const long data_length = (long)&edata - (long)&data_start; // データ領域のサイズ
+  memcpy(&data_start, &erodata, data_length); // ROM領域中の rom の終端からRAMまでごっそりコピーする
   //TODO ここよくわからん。rodataセクションの終端から dataの初期値が入ってるってこと？
 
-	const long bss_length = (long)&ebss - (long)&bss_start; // BSS領域のサイズ。
-	memset(&bss_start, 0, bss_length); //初期化されてない静的領域なので0で初期化してあげる
+  const long bss_length = (long)&ebss - (long)&bss_start; // BSS領域のサイズ。
+  memset(&bss_start, 0, bss_length); //初期化されてない静的領域なので0で初期化してあげる
 
   /** シリアルの初期化 */
-	serial_init(SERIAL_DEFAULT_DEVICE);
+  serial_init(SERIAL_DEFAULT_DEVICE);
 
-	return 0;
+  return 0;
 }
 
 // buf を bytes だけダンプする
 static int dump_h(unsigned char *buf, long bytes){
-	if(bytes < 0){
-		puts("no data.\n");
-		return -1;
-	}
+  if(bytes < 0){
+    puts("no data.\n");
+    return -1;
+  }
 
-	int i = 0;
-	for( i = 0; i < bytes; i++ ){
-		putxval(buf[i], 2);
-		if( (i & 0xF) == 0xF ) puts("\n"); // 16バイトごとに改行
-		if( (i & 0xF) == 0x7 ) puts(" ");  //  8バイトごとにスペース挿入
-	}
-	puts("\n");
+  int i = 0;
+  for( i = 0; i < bytes; i++ ){
+    putxval(buf[i], 2);
+    if( (i & 0xF) == 0xF ) puts("\n"); // 16バイトごとに改行
+    if( (i & 0xF) == 0x7 ) puts(" ");  //  8バイトごとにスペース挿入
+  }
+  puts("\n");
 
-	return 0;
+  return 0;
 }
 
 // wait is 300000
 static void wait(const long wait){
-	volatile long i = 0;
-	for( i = 0; i < wait; i++);
+  volatile long i = 0;
+  for( i = 0; i < wait; i++);
 }
 
 static int receive_program(unsigned char* buf){
-	unsigned char *loadbuf = (unsigned char*)buf;
-	long size = xmodem_recv(loadbuf);
-	wait(300000);
+  unsigned char *loadbuf = (unsigned char*)buf;
+  long size = xmodem_recv(loadbuf);
+  wait(300000);
 
-	if( size < 0 ){ //受信失敗
-		puts("\nXMODEM receive error!\n");
-		return -1;
-	}
+  if( size < 0 ){ //受信失敗
+    puts("\nXMODEM receive error!\n");
+    return -1;
+  }
 
-	puts("\nXMODEM receive success!\n");
+  puts("\nXMODEM receive success!\n");
 
-	return size;
+  return size;
 }
 
 
 int main(void) {
 
-	extern int program_buffer;
-	memset(&program_buffer, 0x00, 0x001d00);
+  extern int program_buffer;
+  memset(&program_buffer, 0x00, 0x001d00);
 
-	init();
+  init();
 
-	puts("kzload (kozos boot loader) started.\n");
+  puts("kzload (kozos boot loader) started.\n");
 
-	while(1){
+  while(1){
 
-		static long program_size = -1;
+    static long program_size = -1;
 
-		#define CommandSize 16
-		static char command_buff[CommandSize];
-		memset(command_buff, 0x00, CommandSize);
+    #define CommandSize 16
+    static char command_buff[CommandSize];
+    memset(command_buff, 0x00, CommandSize);
 
-		puts("kzload> ");
-		gets(command_buff);
+    puts("kzload> ");
+    gets(command_buff);
 
-		#define MATCH(command) (strcmp(command_buff,command) == 0)
+    #define MATCH(command) (strcmp(command_buff,command) == 0)
 
-		if(MATCH("load")){ // XMODEMでファイルをダウンロード
-			memset(&program_buffer, 0x00, 0x001d00);
-			program_size = receive_program((unsigned char*)&program_buffer);
-		}
-		else if(MATCH("dump")){
-			puts("size: "); putxval(program_size, 12);
-			puts("\n");
-			dump_h((unsigned char*)&program_buffer, program_size);
-		}
-		else if(MATCH("run")) {
+    if(MATCH("load")){ // XMODEMでファイルをダウンロード
+      memset(&program_buffer, 0x00, 0x001d00);
+      program_size = receive_program((unsigned char*)&program_buffer);
+    }
+    else if(MATCH("dump")){
+      puts("size: "); putxval(program_size, 12);
+      puts("\n");
+      dump_h((unsigned char*)&program_buffer, program_size);
+    }
+    else if(MATCH("run")) {
       elf_load((unsigned char*)&program_buffer);
-	  }
-		else {
-			puts("unknown command.\n");
-		}
+    }
+    else {
+      puts("unknown command.\n");
+    }
 
-	} // end of while(1)
+  } // end of while(1)
 
-	return 0;
+  return 0;
 }
