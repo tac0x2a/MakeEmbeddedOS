dae1df7e03303a1ee50b5c30a57befd066d1f43f
TAC <tac@tac42.net>
2014-08-27 01:02:10 +0900

    os以下にできるelfファイルをpushするので移動した

---
+++ os/push.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+./h8_tools/bin/kz_xmodem os/tacos.elf /dev/cu.PL2303-*
+./connect.sh
--- push.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-./h8_tools/bin/kz_xmodem bootloader/kzload.elf /dev/cu.PL2303-*
-./connect.sh
